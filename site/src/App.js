057e03daa75dea6766494f4f62399be453776da2
Isaac Johnson <isaacrj.mk@gmail.com>
2024-04-10 20:10:49 -0500

    Add and create home page

--- site/src/App.js
+++ site/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import BG from './images/background.jpg'
 
 const App = () => {
     return (
-        <div>
+        <div className='home-main'>
             <Header/>
-            <h1>Body</h1>
+
+
+                <div className='image' style={{backgroundImage: `url('${BG}')`}}>
+                    <div className='home-content'>
+                        <div className='heading-text'>
+                            <h1>Welcome to</h1>
+                            <h1 className='special-font'>Power Company!</h1>
+                        </div>
+                        <h4>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </h4>
+                    </div>
+                </div>
+
+
             <Footer/>
         </div>
     );
--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import logo from '../images/headerLogo.png';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='header-main'>
+            <img className='header-logo' src={logo} alt='Power Co'/>
+            <div className='navBar'>
+                <h2 className='selected'><a href="#">Home</a></h2>
+                <h2 className=''><a href="#">Our Mission</a></h2>
+                <h2 className=''><a href="#">Reach Out</a></h2>
+                <h2 className=''><a href="#">Volunteer</a></h2>
+            </div>
         </div>
     );
 }
