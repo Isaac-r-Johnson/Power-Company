e3aed4f399d2722dc35f3b33669c048e8d28e140
Isaac Johnson <isaacrj.mk@gmail.com>
2024-04-17 13:59:03 -0500

    design and create 404 page + make some minor styling edits

--- site/src/pages/NotFound.js
+++ site/src/pages/NotFound.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import BG from "../images/background.jpg";
 
 const NotFound = () => {
-    return (
-        <div className=''>
-            <h1>404 Error!</h1>
+  return (
+      <div className="image" style={{ backgroundImage: `url('${BG}')` }}>
+        <div className="error-content">
+          <h1>404 PAGE NOT FOUND!</h1>
+          <h5>
+            The page you are looking for can not be located. <br/> Please check to
+            make sure there are no typos and try again.
+          </h5>
         </div>
-    );
-}
+    </div>
+  );
+};
 
 export default NotFound;
